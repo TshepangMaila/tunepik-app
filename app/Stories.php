4d6749dd989060a9d739b7f6dbbf2ac49065bd6f
MaddoxMaila <37212622+MaddoxMaila@users.noreply.github.com>
2021-04-20 12:49:50 +0200

    Updated Stories Model

--- app/Stories.php
+++ app/Stories.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Stories extends Model
 {
     //
+	protected $table = 'stories';
+	protected $primaryKey = 'story_id';
+	protected $fillable = ['user_id', 'story_message', 'story_url', 'story_type', 'story_time', 'story_date', 'story_id'];
+	public $timestamps = false;
 }
