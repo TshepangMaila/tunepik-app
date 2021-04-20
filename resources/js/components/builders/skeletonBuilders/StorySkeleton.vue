0850ecac8d17530872dba32c963f3d8e855e102b
MaddoxMaila <37212622+MaddoxMaila@users.noreply.github.com>
2021-04-20 11:55:14 +0200

    Started with stories feature implementation!, starting with the front-end side of things

---
+++ resources/js/components/builders/skeletonBuilders/StorySkeleton.vue
@@ -0,0 +1,54 @@
+<template>
+
+	<div class="wrapper">
+		
+		<Flickity ref="flickity" :options="options">
+			
+			<div class="" v-for="(_, i) in [1, 2, 3, 4, 5, 6, 7, 8]" :key="i">
+				
+				<div class="story-skeleton-wrap">
+					<div class="img-shade skeleton-shimmer skeleton-image"></div>
+				</div>
+
+			</div>
+
+		</Flickity>
+
+	</div>
+	
+</template>
+
+<script type="text/javascript">
+
+import Flickity from 'vue-flickity'
+
+	export default {
+
+		name : 'StorySkeleton',
+		data : () => ({
+
+      wrapper  : '.main-carousel',
+      options : {
+    	 					
+				freeScroll : false,
+  	  	contain : true,
+       	wrapAround : true,
+       	autoPlay : 4000,
+       	prevNextButtons : false,
+       	pageDots : false
+
+			}
+
+    }),
+    components : {
+    	Flickity
+    }
+
+
+	};
+	
+</script>
+
+<style type="text/css" scoped>
+	
+</style>
